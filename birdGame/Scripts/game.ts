b859a44ce3b53e93193c58aabb7c39cb1986c4ff
sagarmpatel2161992 <sagarmpatel2161992@yahoo.com>
2015-03-19 14:14:00 -0700

    directory structure has been created

---
+++ birdGame/Scripts/game.ts
@@ -0,0 +1,143 @@
+﻿/// <reference path="typings/createjs-lib/createjs-lib.d.ts" />
+/// <reference path="typings/easeljs/easeljs.d.ts" />
+/// <reference path="typings/tweenjs/tweenjs.d.ts" />
+/// <reference path="typings/soundjs/soundjs.d.ts" />
+/// <reference path="typings/preloadjs/preloadjs.d.ts" />
+/// <reference path="typings/stats/stats.d.ts" />
+
+/// <reference path="constants.ts" />
+/// <reference path="objects/gameobject.ts" />
+/// <reference path="objects/plane.ts" />
+/// <reference path="objects/island.ts" />
+/// <reference path="objects/cloud.ts" />
+/// <reference path="objects/ocean.ts" />
+
+/// <reference path="objects/label.ts" />
+/// <reference path="objects/button.ts" />
+/// <reference path="objects/scoreboard.ts" />
+
+/// <reference path="states/gameover.ts" />
+/// <reference path="states/play.ts" />
+/// <reference path="states/menu.ts" />
+
+
+
+
+// Game Variables
+var stats: Stats = new Stats();
+var canvas;
+var stage: createjs.Stage;
+var assetLoader: createjs.LoadQueue;
+
+// Score Variables
+var finalScore: number = 0;
+var highScore: number = 0;
+
+// State Variables
+var currentState: number;
+var currentStateFunction: any;
+var stateChanged: boolean = false;
+
+
+// Game Objects
+var gameOver: states.GameOver;
+var play: states.Play;
+var menu: states.Menu;
+
+
+// asset manifest - array of asset objects
+var manifest = [
+    { id: "cloud", src: "assets/images/cloud.png" },
+    { id: "island", src: "assets/images/island.png" },
+    { id: "ocean", src: "assets/images/ocean.gif" },
+    { id: "plane", src: "assets/images/plane.png" },
+    { id: "tryAgainButton", src: "assets/images/tryAgainButton.png" },
+    { id: "playButton", src: "assets/images/playButton.png" },
+    { id: "engine", src: "assets/audio/engine.ogg" },
+    { id: "yay", src: "assets/audio/yay.ogg" },
+    { id: "thunder", src: "assets/audio/thunder.ogg" }
+
+];
+
+// Game Objects 
+
+function preload() {
+    assetLoader = new createjs.LoadQueue(); // instantiated assetLoader
+    assetLoader.installPlugin(createjs.Sound);
+    assetLoader.on("complete", init, this); // event handler-triggers when loading done
+    assetLoader.loadManifest(manifest); // loading my asset manifest
+}
+
+
+function init() {
+    canvas = document.getElementById("canvas");
+    stage = new createjs.Stage(canvas);
+    stage.enableMouseOver(20); // Enable mouse events
+    createjs.Ticker.setFPS(60); // 60 frames per second
+    createjs.Ticker.addEventListener("tick", gameLoop);
+    setupStats();
+
+    currentState = constants.MENU_STATE;
+    changeState(currentState);
+}
+
+// UTILITY METHODS +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+function setupStats() {
+    stats.setMode(0);
+    stats.domElement.style.position = 'absolute';
+    stats.domElement.style.left = '650px';
+    stats.domElement.style.top = '440px';
+    document.body.appendChild(stats.domElement);
+}
+
+
+
+
+function gameLoop() {
+    stats.begin(); // Begin metering
+
+    currentStateFunction.update();
+
+
+    if (stateChanged) {
+        changeState(currentState);
+    }
+
+    stage.update(); // Refreshes our stage
+
+    stats.end(); // End metering
+}
+
+
+
+
+
+// Our Game Kicks off in here
+function changeState(state: number) {
+
+    stateChanged = false;
+    switch (state) {
+        case constants.MENU_STATE:
+            // Instantiate Menu State
+            menu = new states.Menu();
+            currentStateFunction = menu;
+            break;
+        case constants.PLAY_STATE:
+            // Instantiate Play State
+            play = new states.Play();
+            currentStateFunction = play;
+            break;
+        case constants.GAME_OVER_STATE:
+            // Instantiate Game Over State
+            gameOver = new states.GameOver();
+            currentStateFunction = gameOver;
+            break;
+    }
+
+
+
+   
+
+
+    
+}
